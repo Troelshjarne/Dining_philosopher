53f86de162dedcb314b4ce43030f0c202273c3ed
troels <trhj@itu.dk>
2021-09-12 12:25:27 +0200

    query

--- Philosophers.go
+++ Philosophers.go
@@ -2,12 +2,10 @@ package main
 
 import "fmt"
 
-//var eatCount  map key philID value = count
-// array of philosopher_Input channels
 var P_OUT [PHILOSOPHERS]chan int
 
 // array of philosopher_output channels
-//var P_OUT [PHILOSOPHERS]chan int
+var P_IN [PHILOSOPHERS]chan int
 
 // each philosopher must include two channels
 //(one for input and one for output, both usable from the outside)
@@ -18,18 +16,16 @@ func InitPhilChans() {
 	}
 
 	// test  if input is correctly inserted into channels
-	a := <-P_OUT[0]
-	b := <-P_OUT[1]
-	c := <-P_OUT[2]
-	d := <-P_OUT[3]
-	e := <-P_OUT[4]
 
-	fmt.Println(a, b, c, d, e)
 }
 
+// take a phil_id and answers time eaten and status
 func AnswerQuery(PhilID int) {
 	status := <-P_OUT[PhilID]
-	fmt.Println("status", status)
+
+	if status == 1 {
+		fmt.Println("thinking", status)
+	}
 
 }
 
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	go InitPhilChans()
 	go DefaultValue()
 
+	go AnswerQuery(0)
 	time.Sleep(time.Millisecond * 1500)
 
 }
